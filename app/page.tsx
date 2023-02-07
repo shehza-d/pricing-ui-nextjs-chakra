9ceeb1ca6990091e3ce6b8d3ef75ac17ee2fdc5a
Shehzad <shehzaddiqbal@gmail.com>
2023-02-07 17:16:02 +0500

    components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
-import MyUI from "../components/MyUI";
+"use client";
+
+import Features from "@/components/Features";
+import Header from "@/components/Header";
+import Pricing from "@/components/Pricing";
+
 // import { Inter } from "@next/font/google";
 
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <MyUI />;
+  return (
+    <>
+      <Header />
+      <Pricing />
+      <Features />
+       {/* <Container
+        as="section"
+        sx={{ border: "2px solid red", ":hover": { color: "red" } }}
+      ></Container>
+      <Box as="main">fd</Box>
+      <Container as="section">
+        <Text>@shehzad footer</Text>
+      </Container> */}
+    </>
+  );
 }
